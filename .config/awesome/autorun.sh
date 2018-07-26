e7657c26655034381eabbd7b2a63dedab30f3eae
Klemen <roxor1992@gmail.com>
2018-07-26 07:46:38 +0200

    Screan layaout oader fix

--- .config/awesome/autorun.sh
+++ .config/awesome/autorun.sh
@@ -11,7 +11,7 @@ run volumeicon
 run cbaticon
 
 if xrandr | grep -q 'HDMI-0 connected' ; then
-	xrandr --output HDMI-0 --primary --mode 1920x1200 --pos 0x0 --rotate normal --output DP-4 --off --output DP-3 --off --output DP-2 --off --output DP-1 --off --output DP-0 --mode 1920x1080 --pos 1920x120 --rotate normal
+	sh .screenlayout/startup.sh
 fi
 
 # vim: tabstop=3 noexpandtab shiftwidth=3 softtabstop=3
