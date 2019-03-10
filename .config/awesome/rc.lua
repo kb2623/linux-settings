d589f6d9e1d08d7ee9044e9ba8808be857ce3a91
KlemenLapTop <roxor1992@gmail.com>
2019-03-10 21:46:31 +0100

    Small fixes

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -22,9 +22,11 @@ require(".zshHelp")
 -- Check if awesome encountered an error during startup and fell back to
 -- another config (This code will only ever execute for the fallback config)
 if awesome.startup_errors then
-	naughty.notify({ preset = naughty.config.presets.critical,
+	naughty.notify({
+			preset = naughty.config.presets.critical,
 			title = "Oops, there were errors during startup!",
-		text = awesome.startup_errors })
+			text = awesome.startup_errors
+		})
 end
 
 -- Handle runtime errors after startup
