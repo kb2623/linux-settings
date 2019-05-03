fe2b0bade6f524351e9a73dd87ae5d51dd1a5056
KlemenLapTop <roxor1992@gmail.com>
2019-05-03 11:57:23 +0200

    Added new terminal to use

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -53,7 +53,7 @@ local theme_path = string.format("%s/.config/awesome/themes/%s/theme.lua", os.ge
 beautiful.init(theme_path)
 
 -- This is used later as the default terminal and editor to run.
-terminal = "xterm"
+terminal = "kitty"
 editor = os.getenv("EDITOR") or "nano"
 editor_cmd = terminal .. " -e " .. editor
 
