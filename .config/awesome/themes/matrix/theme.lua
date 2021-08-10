b1225ba441f377eb784f8c164fe077238b497c26
kb2623 <roxor1992@gmail.com>
2021-08-11 00:46:41 +0200

    Added matrix theme for awesome wm

---
+++ .config/awesome/themes/matrix/theme.lua
@@ -0,0 +1,139 @@
+---------------------------------------------
+-- Awesome theme which follows xrdb config --
+--   by Yauhen Kirylau                    --
+---------------------------------------------
+
+local theme_assets = require("beautiful.theme_assets")
+local xresources = require("beautiful.xresources")
+local wibox = require("wibox")
+local dpi = xresources.apply_dpi
+local xrdb = xresources.get_current_theme()
+local gfs = require("gears.filesystem")
+local themes_path = gfs.get_themes_dir()
+
+-- inherit default theme
+local theme = dofile(themes_path.."default/theme.lua")
+
+theme.confdir       = os.getenv("HOME") .. "/.config/awesome/themes/matrix"
+theme.wallpaper     = theme.confdir .. "/matrix_debian.png"
+theme.font          = "FantasqueSansMono Nerd Font Bold 10"
+
+theme.bg_normal     = xrdb.color0
+theme.bg_focus      = xrdb.color12
+theme.bg_urgent     = xrdb.color9
+theme.bg_minimize   = xrdb.color8
+theme.bg_systray    = theme.bg_normal
+
+theme.fg_normal     = xrdb.foreground
+theme.fg_focus      = theme.bg_normal
+theme.fg_urgent     = theme.bg_normal
+theme.fg_minimize   = theme.bg_normal
+
+theme.useless_gap   = dpi(3)
+theme.border_width  = dpi(1)
+theme.border_normal = xrdb.color8
+theme.border_focus  = theme.bg_focus
+theme.border_marked = xrdb.color10
+
+-- There are other variable sets
+-- overriding the default one when
+-- defined, the sets are:
+-- taglist_[bg|fg]_[focus|urgent|occupied|empty|volatile]
+-- tasklist_[bg|fg]_[focus|urgent]
+-- titlebar_[bg|fg]_[normal|focus]
+-- tooltip_[font|opacity|fg_color|bg_color|border_width|border_color]
+-- mouse_finder_[color|timeout|animate_timeout|radius|factor]
+-- Example:
+--theme.taglist_bg_focus = "#ff0000"
+
+theme.tooltip_fg = theme.fg_normal
+theme.tooltip_bg = theme.bg_normal
+
+-- Variables set for theming the menu:
+-- menu_[bg|fg]_[normal|focus]
+-- menu_[border_color|border_width]
+theme.menu_submenu_icon = themes_path.."default/submenu.png"
+theme.menu_height = dpi(16)
+theme.menu_width  = dpi(150)
+
+-- You can add as many variables as
+-- you wish and access them by using
+-- beautiful.variable in your rc.lua
+--theme.bg_widget = "#cc0000"
+
+-- Recolor Layout icons:
+theme = theme_assets.recolor_layout(theme, theme.fg_normal)
+
+-- Recolor titlebar icons:
+--
+local function darker(color_value, darker_n)
+    local result = "#"
+    for s in color_value:gmatch("[a-fA-F0-9][a-fA-F0-9]") do
+        local bg_numeric_value = tonumber("0x"..s) - darker_n
+        if bg_numeric_value < 0 then bg_numeric_value = 0 end
+        if bg_numeric_value > 255 then bg_numeric_value = 255 end
+        result = result .. string.format("%2.2x", bg_numeric_value)
+    end
+    return result
+end
+theme = theme_assets.recolor_titlebar(
+    theme, theme.fg_normal, "normal"
+)
+theme = theme_assets.recolor_titlebar(
+    theme, darker(theme.fg_normal, -60), "normal", "hover"
+)
+theme = theme_assets.recolor_titlebar(
+    theme, xrdb.color1, "normal", "press"
+)
+theme = theme_assets.recolor_titlebar(
+    theme, theme.fg_focus, "focus"
+)
+theme = theme_assets.recolor_titlebar(
+    theme, darker(theme.fg_focus, -60), "focus", "hover"
+)
+theme = theme_assets.recolor_titlebar(
+    theme, xrdb.color1, "focus", "press"
+)
+
+-- Define the icon theme for application icons. If not set then the icons
+-- from /usr/share/icons and /usr/share/icons/hicolor will be used.
+theme.icon_theme = nil
+
+-- Generate Awesome icon:
+theme.awesome_icon = theme_assets.awesome_icon(
+    theme.menu_height, theme.bg_focus, theme.fg_focus
+)
+
+-- Generate taglist squares:
+local taglist_square_size = dpi(4)
+theme.taglist_squares_sel = theme_assets.taglist_squares_sel(
+    taglist_square_size, theme.fg_normal
+)
+theme.taglist_squares_unsel = theme_assets.taglist_squares_unsel(
+    taglist_square_size, theme.fg_normal
+)
+
+function theme.initBar(s, l, m, r)
+    local left = {}
+    for i,v in ipairs(l) do table.insert(left, v) end
+    local mid = {}
+    for i,v in ipairs(m) do table.insert(mid, v) end
+    local right = {}
+    for i,v in ipairs(r) do table.insert(right, v) end
+    s.mywibox:setup {
+        layout = wibox.layout.align.horizontal,
+        { -- Left widgets
+            layout = wibox.layout.fixed.horizontal,
+            unpack(left),
+        },
+        unpack(mid),
+        { -- Right widgets
+            layout = wibox.layout.fixed.horizontal,
+            unpack(right),
+        },
+    }
+end
+
+return theme
+
+-- vim: filetype=lua:expandtab:shiftwidth=4:tabstop=8:softtabstop=4:textwidth=80
